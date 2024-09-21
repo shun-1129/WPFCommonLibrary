cf41087e5cb20b64bc73759f00b6d137fde2e960
shun-1129 <shun.1129.y@gmail.com>
2024-09-22 01:58:14 +0900

    Resultクラス変更

--- WPFCommonLibrary/Common/Result.cs
+++ WPFCommonLibrary/Common/Result.cs
@@ -1,5 +1,8 @@
 ﻿namespace WPFCommonLibrary.Common
 {
+    /// <summary>
+    /// 結果情報を保持するクラス
+    /// </summary>
     public class Result
     {
         #region メンバ変数
@@ -33,17 +36,6 @@
             Status = false;
             Message = string.Empty;
         }
-
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        /// <param name="inStatus">ステータス</param>
-        /// <param name="inMessage">メッセージ</param>
-        public Result ( bool inStatus , string inMessage )
-        {
-            Status = inStatus;
-            Message = inMessage;
-        }
         #endregion
 
         #region 内部メソッド
@@ -53,11 +45,99 @@
         /// <summary>
         /// 成功
         /// </summary>
-        /// <returns>リザルト</returns>
-        public Result Success ()
+        public void Success ()
         {
-            return new Result ();
+            Status = false;
+            Message = string.Empty;
         }
+
+        /// <summary>
+        /// 失敗
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        public void Failure ( string message )
+        {
+            Status = true;
+            Message = message;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// 結果情報を保持するクラス
+    /// </summary>
+    /// <typeparam name="T">結果情報と共に保持したいデータのクラスを指定</typeparam>
+    public class Result<T>
+    {
+        #region メンバ変数
+        /// <summary>
+        /// 成否
+        /// </summary>
+        private bool _status = false;
+        /// <summary>
+        /// メッセージ内容
+        /// </summary>
+        private string _message = string.Empty;
+        /// <summary>
+        /// ジェネリック
+        /// </summary>
+        private T? _data;
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// 成功／失敗
+        /// </summary>
+        public bool Status { get => _status; private set => _status = value; }
+
+        /// <summary>
+        /// メッセージ
+        /// </summary>
+        public string Message { get => _message; private set => _message = value; }
+
+        /// <summary>
+        /// データ
+        /// </summary>
+        public T? Data { get => _data; private set => _data = value; }
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public Result ()
+        {
+            Status = false;
+            Message = string.Empty;
+            Data = default;
+        }
+        #endregion
+
+        #region 公開メソッド
+        /// <summary>
+        /// 成功
+        /// </summary>
+        /// <param name="data">データ</param>
+        public void Success ( T? data )
+        {
+            Status = false;
+            Message = string.Empty;
+            Data = data;
+        }
+
+        /// <summary>
+        /// 失敗
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        public void Failure ( string message )
+        {
+            Status = true;
+            Message = message;
+            Data = default;
+        }
+        #endregion
+
+        #region 内部メソッド
         #endregion
     }
 }
